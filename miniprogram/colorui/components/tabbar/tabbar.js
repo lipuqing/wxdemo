559317d05cf0889233931a0020af5a261fbdbd36
sean1994 <17638198892@163.com>
2020-10-26 13:44:53 +0800

    页面设置tabbar-page-微调

--- miniprogram/colorui/components/tabbar/tabbar.js
+++ miniprogram/colorui/components/tabbar/tabbar.js
@@ -23,6 +23,10 @@ Component({
       type: [Boolean, String],
       default: false
     },
+    tabIndex: {
+      type: [Number,String],
+      default: 0
+    },
     bgImage: {
       type: String,
       default: ''
@@ -36,22 +40,29 @@ Component({
     CustomBar: app.globalData.CustomBar,
     Custom: app.globalData.Custom,
     icon: [{
-      img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nomine.png?sign=a460593dbf5d6b1daecf02fa68a6bfd7&t=1603334426',
+      img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nofound.png?sign=e65f6596ace68dc9df08046226bb113a&t=1603684202',
+      curr_img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_found.png?sign=c825adbcffa439fa0e3fb2e091496492&t=1603684219',
+      name: '首页',
+      url: '/pages/index/index'
+    },  {
+      img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nofound.png?sign=e65f6596ace68dc9df08046226bb113a&t=1603684202',
+      curr_img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_found.png?sign=c825adbcffa439fa0e3fb2e091496492&t=1603684219',
       name: '发现',
-      url:'/pages/index/index'
+      url: '/pages/index/index'
+    }, {
+      img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nofound.png?sign=e65f6596ace68dc9df08046226bb113a&t=1603684202',
+      curr_img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_found.png?sign=c825adbcffa439fa0e3fb2e091496492&t=1603684219',
+      name: '发现',
+      url: '/pages/index/index'
     }, {
       img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nomine.png?sign=a460593dbf5d6b1daecf02fa68a6bfd7&t=1603334426',
-      name: '发现',
-      url:'/pages/index/index'
-    },{
-      img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nomine.png?sign=a460593dbf5d6b1daecf02fa68a6bfd7&t=1603334426',
-      name: '发现',
-      url:'/pages/index/index'
-    },{
-      img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_nomine.png?sign=a460593dbf5d6b1daecf02fa68a6bfd7&t=1603334426',
+      curr_img: 'https://6474-dtf1v1-c6f838-1255670685.tcb.qcloud.la/icon/icon_mine.png?sign=cc55a7c353d27bf5fb12082a199fad45&t=1603684179',
       name: '我的',
-      url:'/pages/databaseGuide/databaseGuide'
-    }]
+      url: '/pages/index/index'
+    }, ]
+  },
+  onShow(){
+    console.log(this.properties)
   },
   /**
    * 组件的方法列表
@@ -67,7 +78,7 @@ Component({
         url: '/pages/index/index',
       })
     },
-    jump(e){
+    jump(e) {
       console.log(e.currentTarget.dataset.url)
     }
   }
